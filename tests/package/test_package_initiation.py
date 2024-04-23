1148695b8d5ee2d6c281921d9b1af6be0dcee0fd
mohammad amin alavian <m.amin.alavian@gmail.com>
2024-04-23 23:43:30 +0330

    Add metadata parsing to package test

--- tests/package/test_package_initiation.py
+++ tests/package/test_package_initiation.py
@@ -5,5 +5,8 @@ from bssir.metadata_reader import config
 
 
 def test_import_package():
-    config.set_package_config(Path(__file__).parent)
+    _, metadata = config.set_package_config(Path(__file__).parent)
+    metadata.industries
+    metadata.occupations
+    metadata.commodities
     shutil.rmtree(Path(__file__).parent.joinpath("Data_test"))
